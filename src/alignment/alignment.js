2c4433015e6e2cbd238d5a6f0047144722475f96
Donna Murphy <donna.peplinskie@risevision.com>
2014-07-07 10:21:07 -0400

    Style alignment component
    - Add Bootstrap as a dependency
    - Update glyphicons
    
    Implement alignment functionality
    
    - Initialization, getter, setter

--- src/alignment/alignment.js
+++ src/alignment/alignment.js
@@ -9,6 +9,7 @@
 
   function Plugin(element, options) {
     var $element = $(element);
+    var $btnAlignment = null;
 
     options = $.extend({}, { "alignment": "left" }, options);
 
@@ -17,18 +18,34 @@
      */
     function _init() {
       // Get the HTML markup from the template.
-      $element.append(TEMPLATES["alignment-template.html"]);
+      $element.append(TEMPLATES["alignment.html"]);
+      $btnAlignment = $(".btn-alignment");
+
+      $element.find(".dropdown-menu button").on("click", function() {
+        setAlignment($(this).data("wysihtml5-command-value"));
+      });
     }
 
     /*
      *  Public Methods
      */
     function getAlignment() {
-
+     return $btnAlignment.data("wysihtml5-command-value");
     }
 
     function setAlignment(alignment) {
+      var $primaryIcon = $(".btn-alignment .glyphicon");
+      var currentClass = $primaryIcon.attr("class").match(/glyphicon-align-[a-z]+/g);
+      var newClass = "glyphicon-align-" + alignment;
 
+      // Remove current alignment icon.
+      if (currentClass && currentClass.length > 0) {
+        $primaryIcon.removeClass(currentClass[0]);
+      }
+
+      // Add new alignment icon.
+      $primaryIcon.addClass(newClass);
+      $btnAlignment.data("wysihtml5-command-value", alignment);
     }
 
     _init();
