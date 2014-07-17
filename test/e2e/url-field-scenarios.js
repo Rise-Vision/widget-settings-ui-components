393fc0ce40421f831a3f08c1086ba1a3f2bf7c57
Stuart Lees <stuartlees@gmail.com>
2014-07-17 17:08:57 -0400

    URL Field jQuery plugin
    - Removed unnecessary URL field CSS
    - URL field jQuery plugin code
    - URL Field jQuery e2e test HTML
    - Some initial URL Field jQuery e2e tests

--- test/e2e/url-field-scenarios.js
+++ test/e2e/url-field-scenarios.js
@@ -1,8 +1,11 @@
-casper.test.begin("font size picker: size list", function (test) {
-  casper.start("http://localhost:8099/test/e2e/url-input-test.html", function () {
-    // test.assertTitle("Font Size Picker - Test Page", "Page title is the one expected");
-    // test.assertSelectorHasText(".filter-option", "18", "Default size is 18");
-    //TODO
+casper.test.begin("Testing URL Field", function (test) {
+  casper.start("http://localhost:8099/test/e2e/url-field-test.html", function () {
+    test.assertTitle("URL Field - Test Page", "Page title is the one expected");
+    test.assertExists("input[name='url']","URL input field exists");
+    test.assertEval(function () {
+      return document.querySelector("input[name='url']").value === "http://";
+    }, "Default url value is http://");
+    test.assertNotVisible(".validate-url", "Validate URL container is not visible");
   });
 
   casper.run(function() {
