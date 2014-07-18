ea291736027dca3cb33a11b763a85eb2622bf3fc
Stuart Lees <stuartlees@gmail.com>
2014-07-18 13:05:40 -0400

    e2e tests for URL Field component
    - Testing initialized state
    - Testing entering a valid URL and validating
    - Testing entering an invalid URL and validating
    - Testing unchecking the 'Validate URL' checkbox with an invalid URL and validating
    - Testing checking the 'Validate URL' checkbox with an invalid URL and validating

--- test/e2e/url-field-scenarios.js
+++ test/e2e/url-field-scenarios.js
@@ -1,11 +1,71 @@
-casper.test.begin("Testing URL Field", function (test) {
-  casper.start("http://localhost:8099/test/e2e/url-field-test.html", function () {
+var testUrl = "http://localhost:8099/test/e2e/url-field-test.html";
+
+casper.test.begin("URL Field: Initialize", function (test) {
+  casper.start(testUrl, function () {
     test.assertTitle("URL Field - Test Page", "Page title is the one expected");
     test.assertExists("input[name='url']","URL input field exists");
     test.assertEval(function () {
       return document.querySelector("input[name='url']").value === "http://";
     }, "Default url value is http://");
-    test.assertNotVisible(".validate-url", "Validate URL container is not visible");
+    test.assertNotVisible(".validate-url", "'Validate URL' checkbox is not visible");
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+casper.test.begin("URL Field: Enter a valid URL", function (test) {
+  casper.start(testUrl);
+
+  casper.then(function () {
+    this.click("button#setValidUrl");
+    this.click("button#validate");
+    test.assertSelectorHasText(".alert","URL is valid!", "URL should be valid");
+    test.assertNotVisible(".validate-url", "'Validate URL' checkbox is not visible");
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+casper.test.begin("URL Field: Enter an invalid URL", function (test) {
+  casper.then(function () {
+    this.click("button#setInvalidUrl");
+    this.click("button#validate");
+    test.assertSelectorHasText(".alert","URL is invalid.", "URL should be invalid");
+    test.assertVisible(".validate-url", "'Validate URL' checkbox is visible");
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+casper.test.begin("URL Field: Uncheck 'Validate URL' checkbox", function (test) {
+  casper.then(function () {
+    this.click("button#setInvalidUrl");
+    this.click("button#validate");
+    this.click("input[name='validate-url']");
+    this.click("button#validate");
+    test.assertSelectorHasText(".alert","URL is valid!", "URL should be valid");
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+casper.test.begin("URL Field: Check 'Validate URL' checkbox", function (test) {
+  casper.then(function () {
+    this.click("button#setInvalidUrl");
+    this.click("button#validate");
+    this.click("input[name='validate-url']");
+    this.click("button#validate");
+    this.click("input[name='validate-url']");
+    this.click("button#validate");
+    test.assertSelectorHasText(".alert","URL is valid!", "URL should be invalid");
   });
 
   casper.run(function() {
