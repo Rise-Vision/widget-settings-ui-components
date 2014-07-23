ac76e6185b960683fb450cecef2e43a6bce135e2
Donna Murphy <donna.peplinskie@risevision.com>
2014-07-23 11:29:09 -0400

    Add more test cases for alignment plugin

--- test/e2e/alignment-scenarios.js
+++ test/e2e/alignment-scenarios.js
@@ -1,16 +1,24 @@
-casper.test.begin("Testing alignment", function (test) {
-  casper.start("http://localhost:8099/test/e2e/alignment-test.html", function () {
-    test.assertTitle("Alignment - Test Page", "Test page has loaded");
-    test.assertExists(".btn-alignment .glyphicon-align-left",
-      "Icon is set to left align");
-    test.assertExists(".btn-alignment[data-wysihtml5-command-value='left']",
-      "Data attribute is set to 'left'");
-    test.assertNotVisible(".dropdown-menu", "Dropdown is not visible");
-  });
+casper.test.begin("Alignment - Loading", function (test) {
+  casper.start("http://localhost:8099/test/e2e/alignment-test.html",
+    function () {
+      test.assertTitle("Alignment - Test Page", "Test page has loaded");
+      test.assertExists(".btn-alignment .glyphicon-align-left",
+        "Icon is set to left");
+      test.assertExists(".btn-alignment[data-wysihtml5-command-value='left']",
+        "Data attribute is set to left");
+      test.assertEval(function () {
+        return $("#alignment").data("plugin_alignment").getAlignment() === "left";
+      }, "getAlignment returns left");
+      test.assertNotVisible(".dropdown-menu", "Dropdown is not visible");
+    }
+  );
 
   // Open the dropdown menu.
   casper.then(function () {
+    casper.test.comment("Alignment - Open the dropdown menu");
+
     this.click(".btn-alignment");
+
     test.assertVisible(".dropdown-menu", "Dropdown is visible");
     test.assertElementCount(".dropdown-menu button", 4,
       "4 alignment options listed");
@@ -18,9 +26,20 @@ casper.test.begin("Testing alignment", function (test) {
 
   // Click on justify button.
   casper.then(function () {
+    casper.test.comment("Alignment - Justify");
+
     this.click(".dropdown-menu button[data-wysihtml5-command-value='justify']");
+
     test.assertExists(".btn-alignment .glyphicon-align-justify",
       "Icon is set to justify");
+    test.assertEval(function () {
+      /* Need to use jQuery to get the data attribute since it doesn't update in
+         the DOM. */
+      return $(".btn-alignment").data("wysihtml5-command-value") === "justify";
+    }, "Data attribute is set to justify");
+    test.assertEval(function () {
+      return $("#alignment").data("plugin_alignment").getAlignment() === "justify";
+      }, "getAlignment returns justify");
     test.assertNotVisible(".dropdown-menu", "Dropdown is not visible");
   });
 
