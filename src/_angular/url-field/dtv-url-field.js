6a48d82327704235391bc85ad8384b4dc56c0bc1
Stuart Lees <stuartlees@gmail.com>
2014-08-28 17:26:24 -0400

    Refactored directive version of URL Field
    - Simplified directive and HTML template to better leverage Angular api
    - Updated with self validating logic and providing an invalid message
    - translation.json updated with invalid message
    - Optional ngModel to apply $setValidity from parent controller

--- src/_angular/url-field/dtv-url-field.js
+++ src/_angular/url-field/dtv-url-field.js
@@ -2,87 +2,72 @@
   "use strict";
 
   angular.module("risevision.widget.common.url-field", ["risevision.widget.common.translate"])
-    .directive("urlField", ["$templateCache", function ($templateCache) {
+
+    .directive("urlField", ["$templateCache", "$log", function ($templateCache, $log) {
       return {
         restrict: "E",
+        require: "?ngModel",
         scope: {
           url: "="
         },
         template: $templateCache.get("_angular/url-field/url-field.html"),
-        link: function (scope, element) {
-          var doValidation = true,
-            $el = $(element),
-            $urlInp = $el.find("input[name='url']"),
-            $validateUrlCtn = $el.find("div.validate-url"),
-            $validateUrlCB = $el.find("input[name='validate-url']"),
-            urlRegExp;
-
-          /* jshint ignore:start */
-          urlRegExp = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
-          /* jshint ignore:end */
-
-          function getUrl() {
-            return $.trim($urlInp.val());
-          }
+        link: function (scope, element, attrs, ctrl) {
 
           function testUrl(value) {
-            // Add http:// if no protocol parameter exists
-            if (value.indexOf("://") === -1) {
-              value = "http://" + value;
-            }
+            var urlRegExp;
+
             /*
              Discussion
-             http://stackoverflow.com/questions/37684/how-to-replace-plain-urls-
-             with-links#21925491
+             http://stackoverflow.com/questions/37684/how-to-replace-plain-urls-with-links#21925491
 
              Using
              https://gist.github.com/dperini/729294
              Reasoning
-             http://mathiasbynens.be/demo/url-regex
+             http://mathiasbynens.be/demo/url-regex */
 
-             */
+            /* jshint ignore:start */
+            urlRegExp = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+            /* jshint ignore:end */
+
+            // Add http:// if no protocol parameter exists
+            if (value.indexOf("://") === -1) {
+              value = "http://" + value;
+            }
 
             return urlRegExp.test(value);
           }
 
-          function setUrl(value) {
-            if (typeof value === "string") {
-              $urlInp.val(value);
-            }
-          }
+          // By default enforce validation
+          scope.doValidation = true;
+          // A count of fails to correspond with template on showing validate checkbox
+          scope.failCount = 0;
+          // Validation state
+          scope.valid = true;
 
-          function validateUrl() {
-            if (!doValidation) {
-              return true;
-            }
-
-            var valid = testUrl(getUrl());
+          scope.$watch("valid", function (valid) {
             if (!valid) {
-              if (!$validateUrlCtn.is(":visible")) {
-                $validateUrlCtn.show();
-              }
+              scope.failCount += 1;
             }
 
-            return valid;
-          }
-
-          scope.$watch("url", function (url) {
-            if (typeof url === "string") {
-              setUrl(url);
+            if (ctrl) {
+              $log.info("Calling $setValidity() on parent controller");
+              ctrl.$setValidity("valid", valid);
             }
           });
 
-          scope.$on("collectAdditionalParams", function () {
-            if (validateUrl()) {
-              scope.url = getUrl();
+          scope.$watch("doValidation", function (doValidation) {
+            if (doValidation && typeof scope.url !== "undefined" ) {
+              scope.valid = testUrl(scope.url);
+            } else {
+              scope.valid = true;
             }
           });
 
-          $validateUrlCB.on("click", function () {
-            doValidation = $(this).is(":checked");
-          });
-
-          $validateUrlCtn.hide();
+          scope.onChange = function () {
+            if (scope.doValidation) {
+              scope.valid = testUrl(scope.url);
+            }
+          };
         }
       };
     }]);
