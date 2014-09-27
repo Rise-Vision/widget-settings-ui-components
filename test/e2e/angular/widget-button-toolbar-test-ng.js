57c4e7edf7493820e41d1d3db51b5d618ab8c1e0
Stuart Lees <stuartlees@gmail.com>
2014-09-26 20:04:32 -0400

    Widget Button Toolbar e2e tests
    - e2e HTML updated to provide attribute values to the directive, test elements added and testapp `$scope` logic added for running tests
    - e2e tests added

--- test/e2e/angular/widget-button-toolbar-test-ng.js
+++ test/e2e/angular/widget-button-toolbar-test-ng.js
@@ -8,6 +8,9 @@
   var chaiAsPromised = require("chai-as-promised");
 
   chai.use(chaiAsPromised);
+  var expect = chai.expect;
+
+  var fs = require("fs");
 
   browser.driver.manage().window().setSize(1024, 768);
 
@@ -16,7 +19,43 @@
       browser.get("/test/e2e/angular/widget-button-toolbar-test-ng.html");
     });
 
-    // TODO: e2e tests to come
+    it("should load correctly", function () {
+      // save button should be enabled
+      expect(element(by.css("button#save[disabled=disabled")).isPresent()).
+        to.eventually.be.false;
+
+      expect(element(by.css("button#invalidTestBtn")))
+    });
+
+    it("should execute save function on click", function () {
+      element(by.css("button#save")).click();
+
+      expect(element(by.id("testInput")).getAttribute("value")).
+        to.eventually.equal("Save button clicked");
+    });
+
+    it("should execute cancel function on click", function () {
+      element(by.css("button#cancel")).click();
+
+      expect(element(by.id("testInput")).getAttribute("value")).
+        to.eventually.equal("Cancel button clicked");
+    });
+
+    it("should execute disabling save button", function () {
+      element(by.css("button#invalidTestBtn")).click();
+
+      // save button should be disabled
+      expect(element(by.css("button#save[disabled=disabled")).isPresent()).
+        to.eventually.be.true;
+    });
+
+    it("should execute enabling save button", function () {
+      element(by.css("button#validTestBtn")).click();
+
+      // save button should be disabled
+      expect(element(by.css("button#save[disabled=disabled")).isPresent()).
+        to.eventually.be.false;
+    });
 
   });
 
