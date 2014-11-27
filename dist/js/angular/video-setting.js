6af8c2c727771ef1ac90d773bfd56622357a1ee3
Stuart Lees <stuartlees@gmail.com>
2014-11-26 20:27:24 -0500

    [Video Setting] Template, directive, and e2e tests
    - HTML template created with future addition of slider component to come
    - Directive added and includes scope property value for volume which will come from future addition of slider component
    - Translations added for template
    - e2e test HTML added and e2e tests added
    - Added missing "build" in "scripts" object of package.json

---
+++ dist/js/angular/video-setting.js
@@ -0,0 +1,71 @@
+(function () {
+  "use strict";
+
+  angular.module("risevision.widget.common.video-setting", ["risevision.widget.common.translate"])
+    .directive("videoSetting", ["$templateCache", "$log", function ($templateCache/*, $log*/) {
+      return {
+        restrict: "E",
+        scope: {
+          video: "="
+        },
+        template: $templateCache.get("_angular/video-setting/video-setting.html"),
+        link: function ($scope) {
+          $scope.defaultSetting = {
+            autoplay: true,
+            volume: 50,
+            loop: true,
+            autohide: true
+          };
+
+          $scope.defaults = function(obj) {
+            if (obj) {
+              for (var i = 1, length = arguments.length; i < length; i++) {
+                var source = arguments[i];
+
+                for (var prop in source) {
+                  if (obj[prop] === void 0) {
+                    obj[prop] = source[prop];
+                  }
+                }
+              }
+            }
+            return obj;
+          };
+
+          $scope.$watch("video", function(video) {
+            $scope.defaults(video, $scope.defaultSetting);
+          });
+
+        }
+      };
+    }]);
+}());
+
+(function(module) {
+try { app = angular.module("risevision.widget.common.video-setting"); }
+catch(err) { app = angular.module("risevision.widget.common.video-setting", []); }
+app.run(["$templateCache", function($templateCache) {
+  "use strict";
+  $templateCache.put("_angular/video-setting/video-setting.html",
+    "<div class=\"section\">\n" +
+    "  <h5>{{\"video.heading\" | translate}}</h5>\n" +
+    "  <div class=\"checkbox\">\n" +
+    "    <label>\n" +
+    "      <input name=\"video-autoplay\" type=\"checkbox\" ng-model=\"video.autoplay\"> {{\"video.autoplay.label\" | translate}}\n" +
+    "    </label>\n" +
+    "  </div>\n" +
+    "  <!-- TODO: Integrate slider component -->\n" +
+    "  <div class=\"checkbox\">\n" +
+    "    <label>\n" +
+    "      <input name=\"video-loop\" type=\"checkbox\" ng-model=\"video.loop\"> {{\"video.loop.label\" | translate}}\n" +
+    "    </label>\n" +
+    "  </div>\n" +
+    "  <div class=\"checkbox\">\n" +
+    "    <label>\n" +
+    "      <input name=\"video-autohide\" type=\"checkbox\" ng-model=\"video.autohide\"> {{\"video.autohide.label\" | translate}}\n" +
+    "    </label>\n" +
+    "  </div>\n" +
+    "</div>\n" +
+    "");
+}]);
+})();
---
+++ src/_angular/video-setting/dtv-video-setting.js
@@ -0,0 +1,42 @@
+(function () {
+  "use strict";
+
+  angular.module("risevision.widget.common.video-setting", ["risevision.widget.common.translate"])
+    .directive("videoSetting", ["$templateCache", "$log", function ($templateCache/*, $log*/) {
+      return {
+        restrict: "E",
+        scope: {
+          video: "="
+        },
+        template: $templateCache.get("_angular/video-setting/video-setting.html"),
+        link: function ($scope) {
+          $scope.defaultSetting = {
+            autoplay: true,
+            volume: 50,
+            loop: true,
+            autohide: true
+          };
+
+          $scope.defaults = function(obj) {
+            if (obj) {
+              for (var i = 1, length = arguments.length; i < length; i++) {
+                var source = arguments[i];
+
+                for (var prop in source) {
+                  if (obj[prop] === void 0) {
+                    obj[prop] = source[prop];
+                  }
+                }
+              }
+            }
+            return obj;
+          };
+
+          $scope.$watch("video", function(video) {
+            $scope.defaults(video, $scope.defaultSetting);
+          });
+
+        }
+      };
+    }]);
+}());
