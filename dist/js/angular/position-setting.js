55f4a92414e38372c45071b717786cda03e64088
Stuart Lees <stuartlees@gmail.com>
2015-01-22 15:35:13 -0500

    Adding Position Setting component
    - HTML template
    - directive source
    - e2e test HTML
    - e2e tests
    - demo

---
+++ dist/js/angular/position-setting.js
@@ -0,0 +1,52 @@
+(function () {
+  "use strict";
+
+  angular.module("risevision.widget.common.position-setting", ["risevision.common.i18n"])
+    .directive("positionSetting", ["$templateCache", "$log", function ($templateCache/*, $log*/) {
+      return {
+        restrict: "E",
+        scope: {
+          position: "="
+        },
+        template: $templateCache.get("_angular/position-setting/position-setting.html"),
+        link: function ($scope) {
+          $scope.$watch("position", function(position) {
+            if (typeof position === "undefined") {
+              // set a default
+              $scope.position = "top-left";
+            }
+          });
+        }
+      };
+    }]);
+}());
+
+(function(module) {
+try { app = angular.module("risevision.widget.common.position-setting"); }
+catch(err) { app = angular.module("risevision.widget.common.position-setting", []); }
+app.run(["$templateCache", function($templateCache) {
+  "use strict";
+  $templateCache.put("_angular/position-setting/position-setting.html",
+    "<div class=\"row\">\n" +
+    "  <div class=\"col-md-3\">\n" +
+    "    <div class=\"form-group\">\n" +
+    "      <label>\n" +
+    "        {{'widgets.alignment' | translate}}\n" +
+    "      </label>\n" +
+    "      <select name=\"position\" ng-model=\"position\" class=\"form-control\">\n" +
+    "        <option value=\"top-left\">{{'position.top.left' | translate}}</option>\n" +
+    "        <option value=\"top-center\">{{'position.top.center' | translate}}</option>\n" +
+    "        <option value=\"top-right\">{{'position.top.right' | translate}}</option>\n" +
+    "        <option value=\"middle-left\">{{'position.middle.left' | translate}}</option>\n" +
+    "        <option value=\"middle-center\">{{'position.middle.center' | translate}}</option>\n" +
+    "        <option value=\"middle-right\">{{'position.middle.right' | translate}}</option>\n" +
+    "        <option value=\"bottom-left\">{{'position.bottom.left' | translate}}</option>\n" +
+    "        <option value=\"bottom-center\">{{'position.bottom.center' | translate}}</option>\n" +
+    "        <option value=\"bottom-right\">{{'position.bottom.right' | translate}}</option>\n" +
+    "      </select>\n" +
+    "    </div>\n" +
+    "  </div>\n" +
+    "</div>\n" +
+    "");
+}]);
+})();
---
+++ test/e2e/angular/position-setting-test-ng.js
@@ -0,0 +1,51 @@
+/* jshint expr: true */
+
+(function() {
+
+  "use strict";
+
+  var chai = require("chai");
+  var chaiAsPromised = require("chai-as-promised");
+
+  chai.use(chaiAsPromised);
+  var expect = chai.expect;
+
+  var fs = require("fs");
+
+  browser.driver.manage().window().setSize(1024, 768);
+
+  describe("Position Setting Component", function() {
+    beforeEach(function (){
+      browser.get("/test/e2e/angular/position-setting-test-ng.html");
+    });
+
+    it("Should correctly load", function () {
+      expect(element(by.css("select[name='position']")).isPresent()).to.eventually.be.true;
+      expect(element(by.css("select[name='position']")).isDisplayed()).to.eventually.be.true;
+    });
+
+    it("Should correctly load with default selection", function () {
+      element.all(by.css("select[name='position'] option")).then(function (elements) {
+        // select element should display 9 options
+        expect(elements.length).to.equal(9);
+      });
+
+      // select element should have correct default option selected
+      expect(element(by.css("select[name='position']")).$('option:checked').getAttribute("value"))
+        .to.eventually.equal("top-left");
+
+      // ng-model 'position' value should be bound to select element and have the correct value
+      expect(element(by.model("position")).getAttribute("value")).to.eventually.equal("top-left");
+    });
+
+    it("Should update the position model value by selecting a different option", function () {
+      element(by.css("select[name='position']")).click();
+      element(by.css("select[name='position'] option[value='middle-right']")).click();
+
+      // ng-model 'position' value should be bound to select element and have the correct value
+      expect(element(by.model("position")).getAttribute("value")).to.eventually.equal("middle-right");
+    })
+
+  });
+
+})();
