ee5631326bd820f005a211af6f630ba52aefb2a1
rodrigopavezi <rodrigopavezi@gmail.com>
2015-12-21 18:11:57 -0200

    Update 2015-12-21T20:11:47.607Z

--- mocks/i18n-config.js
+++ mocks/i18n-config.js
@@ -13,5 +13,5 @@ angular.module("risevision.widget.common", [])
   
 angular.module('risevision.common.i18n.config', [])
   .constant('LOCALES_PREFIX',
-    '/components/rv-common-i18n/dist/locales/translation_')
+    'components/rv-common-i18n/dist/locales/translation_')
   .constant('LOCALES_SUFIX', '.json');
--- mocks/segment-analytics-mock.js
+++
@@ -1,9 +0,0 @@
-(function (angular) {
-
-  "use strict";
-
-  // This mock disables the segment.io tracking script by
-  // resetting the API key
-  angular.module("risevision.common.components.analytics")
-    .value("SEGMENT_API_KEY", null);
-})(angular);
