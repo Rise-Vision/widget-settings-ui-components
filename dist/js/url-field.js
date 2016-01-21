8110417bdf95d4fbf02fcdd0960ed028cad49d68
Donna Peplinskie <donna.peplinskie@risevision.com>
2016-01-21 13:50:03 -0500

    Fix config files

--- dist/js/url-field.js
+++ dist/js/url-field.js
@@ -119,27 +119,24 @@ TEMPLATES['url-field-template.html'] = "<div class=\"form-group validate-url\">\
   };
 })(jQuery, window, document, TEMPLATES);
 
-/* global WIDGET_SETTINGS_UI_CONFIG: true */
 /* exported WIDGET_SETTINGS_UI_CONFIG */
-if (typeof WIDGET_SETTINGS_UI_CONFIG === "undefined") {
-  var WIDGET_SETTINGS_UI_CONFIG = {
-    "families": "Andale Mono=andale mono,monospace;" +
-        "Arial=arial,helvetica,sans-serif;" +
-        "Arial Black=arial black,sans-serif;" +
-        "Book Antiqua=book antiqua,palatino,serif;" +
-        "Comic Sans MS=comic sans ms,sans-serif;" +
-        "Courier New=courier new,courier,monospace;" +
-        "Georgia=georgia,palatino,serif;" +
-        "Helvetica=helvetica,arial,sans-serif;" +
-        "Impact=impact,sans-serif;" +
-        "Symbol=symbol;" +
-        "Tahoma=tahoma,arial,helvetica,sans-serif;" +
-        "Terminal=terminal,monaco,monospace;" +
-        "Times New Roman=times new roman,times,serif;" +
-        "Trebuchet MS=trebuchet ms,geneva,sans-serif;" +
-        "Verdana=verdana,geneva,sans-serif;" +
-        "Webdings=webdings;" +
-        "Wingdings=wingdings,zapf dingbats;",
-    "sizes": "8px 9px 10px 11px 12px 14px 18px 24px 30px 36px 48px 60px 72px 96px"
-  };
-}
+var WIDGET_SETTINGS_UI_CONFIG = {
+  "families": "Andale Mono=andale mono,monospace;" +
+      "Arial=arial,helvetica,sans-serif;" +
+      "Arial Black=arial black,sans-serif;" +
+      "Book Antiqua=book antiqua,palatino,serif;" +
+      "Comic Sans MS=comic sans ms,sans-serif;" +
+      "Courier New=courier new,courier,monospace;" +
+      "Georgia=georgia,palatino,serif;" +
+      "Helvetica=helvetica,arial,sans-serif;" +
+      "Impact=impact,sans-serif;" +
+      "Symbol=symbol;" +
+      "Tahoma=tahoma,arial,helvetica,sans-serif;" +
+      "Terminal=terminal,monaco,monospace;" +
+      "Times New Roman=times new roman,times,serif;" +
+      "Trebuchet MS=trebuchet ms,geneva,sans-serif;" +
+      "Verdana=verdana,geneva,sans-serif;" +
+      "Webdings=webdings;" +
+      "Wingdings=wingdings,zapf dingbats;",
+  "sizes": "8px 9px 10px 11px 12px 14px 18px 24px 30px 36px 48px 60px 72px 96px"
+};
