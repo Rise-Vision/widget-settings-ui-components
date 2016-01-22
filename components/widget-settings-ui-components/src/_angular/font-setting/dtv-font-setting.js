5c1f85ffb21bccd0a85aeba9b44a693b0a008d22
jenkins-rise <jenkins@risevision.com>
2016-01-22 21:49:42 +0000

    Update 2016-01-22T21:49:36.472Z

--- components/widget-settings-ui-components/src/_angular/font-setting/dtv-font-setting.js
+++ components/widget-settings-ui-components/src/_angular/font-setting/dtv-font-setting.js
@@ -1,40 +1,40 @@
+/* global WIDGET_SETTINGS_UI_CONFIG */
 (function () {
   "use strict";
 
-  angular.module("risevision.widget.common.font-setting", ["risevision.common.i18n",
-    "risevision.widget.common.font-style", "risevision.widget.common.alignment",
-    "risevision.widget.common.color-picker", "risevision.widget.common.fontsizepicker",
-    "risevision.widget.common.fontpicker"])
-    .directive("fontSetting", ["$templateCache", function ($templateCache) {
+  angular.module("risevision.widget.common.font-setting", ["angularLoad", "ui.tinymce"])
+    .directive("fontSetting", ["$templateCache", "$log", "googleFontLoader", function ($templateCache, $log, googleFontLoader) {
       return {
         restrict: "AE",
         scope: {
           fontData: "=",
-          previewText: "@",
-          hideAlignment: "@"
+          previewText: "@"
         },
         template: $templateCache.get("_angular/font-setting/font-setting.html"),
         transclude: false,
-        link: function ($scope, element, attrs) {
-          var $element = $(element);
+        link: function ($scope) {
+          var _fontData = null,
+            _googleFontList = "";
 
           $scope.defaultFont = {
             font: {
-              type: "standard",
-              name: "Verdana",
-              family: "Verdana"
+              family: "verdana,geneva,sans-serif",
+              type: "standard"
             },
-            size: "20",
+            size: "24px",
+            align: "left",
             bold: false,
             italic: false,
             underline: false,
-            color: "black",
-            highlightColor: "transparent"
+            forecolor: "black",
+            backcolor: "transparent"
           };
 
-          if (typeof attrs.hideAlignment === "undefined" || attrs.hideAlignment !== "true") {
-            $scope.defaultFont.align = "left";
-          }
+          // Load Google fonts.
+          googleFontLoader.getFonts().then(function(fonts) {
+            _googleFontList = fonts;
+            initTinyMCE(fonts);
+          });
 
           $scope.defaults = function(obj) {
             if (obj) {
@@ -60,22 +60,180 @@
               if ($scope.previewText) {
                 $scope.$watch("fontData", updatePreview, true);
               }
+
+              _fontData = fontData;
             }
           });
 
-          function updatePreview(fontData) {
-            if ($scope.previewText && fontData) {
-              var parentEl = $element.find(".font-picker-text");
-              var previewEl = $element.find(".font-picker-text span");
-              previewEl.css("font-family", fontData.font.family);
-              previewEl.css("font-size", fontData.size + "px");
-              previewEl.css("font-weight", fontData.bold ? "bold" : "normal");
-              previewEl.css("font-style", fontData.italic ? "italic" : "normal");
-              previewEl.css("text-decoration", fontData.underline ? "underline" : "none");
-              previewEl.css("color", fontData.color);
-              previewEl.css("background-color", fontData.highlightColor);
+          // Initialize TinyMCE.
+          function initTinyMCE(families) {
+            $scope.tinymceOptions = {
+              font_formats: WIDGET_SETTINGS_UI_CONFIG.families + families,
+              fontsize_formats: WIDGET_SETTINGS_UI_CONFIG.sizes,
+              menubar: false,
+              plugins: "textcolor",
+              statusbar: false,
+              toolbar: "fontselect fontsizeselect | alignleft aligncenter alignright alignjustify | forecolor backcolor | bold italic underline",
+              setup: function(editor) {
+                editor.on("init", function() {
+                  initToolbar(editor);
+                });
 
-              parentEl.css("text-align", fontData.align);
+                editor.on("ExecCommand", function(args) {
+                  initCommands(args);
+                });
+              },
+              init_instance_callback: function(editor) {
+                var oldApply = editor.formatter.apply;
+
+                // Reference - http://goo.gl/55IhWI
+                editor.formatter.apply = function apply(name, vars, node) {
+                  var args = {
+                    command: name,
+                    value: vars.value
+                  };
+
+                  oldApply(name, vars, node);
+                  editor.fire("ExecCommand", args);
+                };
+              }
+            };
+          }
+
+          // Initialize TinyMCE toolbar.
+          function initToolbar(editor) {
+            if (_fontData) {
+              // Alignment
+              switch(_fontData.align) {
+                case "left":
+                  editor.execCommand("JustifyLeft", false);
+                  break;
+                case "center":
+                  editor.execCommand("JustifyCenter", false);
+                  break;
+                case "right":
+                  editor.execCommand("JustifyRight", false);
+                  break;
+                case "justify":
+                  editor.execCommand("JustifyFull", false);
+                  break;
+                default:
+                  break;
+              }
+
+              // Colors
+              $(".mce-colorbutton[aria-label='Text color'] span").css("background-color", _fontData.forecolor);
+              $(".mce-colorbutton[aria-label='Background color'] span").css("background-color", _fontData.backcolor);
+
+              // Font Style
+              if (_fontData.bold) {
+                toggleButton($(".mce-btn[aria-label='Bold']"));
+              }
+
+              if (_fontData.italic) {
+                toggleButton($(".mce-btn[aria-label='Italic']"));
+              }
+
+              if (_fontData.underline) {
+                toggleButton($(".mce-btn[aria-label='Underline']"));
+              }
+
+              // These 2 must be last for some reason.
+              editor.execCommand("FontName", false, _fontData.font.family);
+              editor.execCommand("FontSize", false, _fontData.size);
+            }
+          }
+
+          // Determine what type of font this is (standard or google)
+          function getFontType(family) {
+            if (WIDGET_SETTINGS_UI_CONFIG.families.indexOf(family) !== -1) {
+              return "standard";
+            }
+
+            if (_googleFontList.indexOf(family) !== -1) {
+              return "google";
+            }
+
+            return "";
+          }
+
+          // Handle toolbar interactions.
+          function initCommands(args) {
+            switch(args.command) {
+              case "FontName":
+                _fontData.font.family = args.value;
+                _fontData.font.type = getFontType(args.value);
+                break;
+
+              case "FontSize":
+                _fontData.size = args.value;
+                break;
+
+              case "JustifyLeft":
+                _fontData.align = "left";
+                break;
+
+              case "JustifyCenter":
+                _fontData.align = "center";
+                break;
+
+              case "JustifyRight":
+                _fontData.align = "right";
+                break;
+
+              case "JustifyFull":
+                _fontData.align = "justify";
+                break;
+
+              case "forecolor":
+                _fontData.forecolor = args.value;
+                break;
+
+              case "hilitecolor":
+                _fontData.backcolor = args.value;
+                break;
+
+              case "mceToggleFormat":
+                if (args.value === "bold") {
+                  _fontData.bold = !_fontData.bold;
+                  toggleButton($(".mce-btn[aria-label='Bold']"));
+                }
+                else if (args.value === "italic") {
+                  _fontData.italic = !_fontData.italic;
+                  toggleButton($(".mce-btn[aria-label='Italic']"));
+                }
+                else if (args.value === "underline") {
+                  _fontData.underline = !_fontData.underline;
+                  toggleButton($(".mce-btn[aria-label='Underline']"));
+                }
+
+                break;
+
+              default:
+                break;
+            }
+
+            updatePreview(_fontData);
+          }
+
+          function toggleButton($btn) {
+            $btn.toggleClass("mce-active");
+          }
+
+          // Style the preview text.
+          function updatePreview(fontData) {
+            var textContainer = document.querySelector(".text-container"),
+              text = document.querySelector(".text");
+
+            if ($scope.previewText && fontData) {
+              text.style.fontFamily = fontData.font.family;
+              text.style.fontSize = fontData.size;
+              text.style.fontWeight = fontData.bold ? "bold" : "normal";
+              text.style.fontStyle = fontData.italic ? "italic" : "normal";
+              text.style.textDecoration = fontData.underline ? "underline" : "none";
+              text.style.color = fontData.forecolor;
+              text.style.backgroundColor = fontData.backcolor;
+              textContainer.style.textAlign = fontData.align;
             }
           }
         }
