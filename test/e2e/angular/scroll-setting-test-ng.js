897ba2baa461a21f064ed06872c6dc44b2b60e5e
rodrigopavezi <rodrigopavezi@gmail.com>
2016-07-06 22:31:49 +0100

    Hide direction from scroll settings when by page is selected Fixed tests

--- test/e2e/angular/scroll-setting-test-ng.js
+++ test/e2e/angular/scroll-setting-test-ng.js
@@ -39,12 +39,12 @@
       expect(element(by.id("scroll-pud")).getAttribute("value")).to.eventually.equal("10");
     });
 
-    it("Should display direction setting when scroll is page", function () {
+    it("Should NOT display direction setting when scroll is page", function () {
       expect(element(by.id("scroll-by")).getAttribute("value")).to.eventually.equal("none");
 
       element(by.cssContainingText('option', 'Page')).click();
 
-      expect(element(by.id("scroll-direction")).isDisplayed()).to.eventually.be.true;
+      expect(element(by.id("scroll-direction")).isDisplayed()).to.eventually.be.false;
     });
 
     it("Should only show speed when direction is set to left", function () {
