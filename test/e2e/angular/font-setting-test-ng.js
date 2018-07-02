7c11617b980b9cbe9e071bccaed49455822d176c
Peter Sandford <peter@cameroncodes.com>
2018-07-02 11:36:13 -0400

    updates test for number of fonts

--- test/e2e/angular/font-setting-test-ng.js
+++ test/e2e/angular/font-setting-test-ng.js
@@ -30,7 +30,7 @@
         it("should load all fonts", function() {
           element(by.css("#font1  .mce-btn[aria-label='Font Family']")).click();
           element.all(by.css("#mceu_42-body div")).then(function(elements) {
-            expect(elements.length).to.equal(889);
+            expect(elements.length).to.be.above(800);
           });
         });
 
@@ -407,7 +407,7 @@
         it("should load all fonts", function() {
           element(by.css("#font2  .mce-btn[aria-label='Font Family']")).click();
           element.all(by.css("#mceu_42-body div")).then(function(elements) {
-            expect(elements.length).to.equal(889);
+            expect(elements.length).to.be.above(800);
           });
         });
 
