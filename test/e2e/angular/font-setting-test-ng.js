0b33559d93598937ad794ba374fe34a4ae6b15da
santiagonoguez <santiago@nul-unu.com>
2018-07-31 10:25:09 -0500

    add delay to test

--- test/e2e/angular/font-setting-test-ng.js
+++ test/e2e/angular/font-setting-test-ng.js
@@ -278,18 +278,24 @@
       describe("Google Font", function() {
         it("should update font family for preview text if family name is one word", function() {
           element(by.css("#font1 .mce-btn[aria-label='Font Family']")).click();
-          element(by.css(".mce-floatpanel")).sendKeys(webdriver.Key.END);
-          element(by.xpath("//span[contains(text(), 'Yrsa')]")).click();
 
-          expect(element(by.css("#font1 .text")).getCssValue("font-family")).to.eventually.equal('Yrsa, sans-serif');
+          return webdriver.wait(function() {
+            element(by.css(".mce-floatpanel")).sendKeys(webdriver.Key.END);
+            element(by.xpath("//span[contains(text(), 'Yrsa')]")).click();
+
+            expect(element(by.css("#font1 .text")).getCssValue("font-family")).to.eventually.equal('Yrsa, sans-serif');
+          }, 1000);
         });
 
         it("should update font family for preview text if family name has spaces and numbers", function() {
           element(by.css("#font1 .mce-btn[aria-label='Font Family']")).click();
-          element(by.css(".mce-floatpanel")).sendKeys(webdriver.Key.END);
-          element(by.xpath("//span[contains(text(), 'Zilla Slab')]")).click();
 
-          expect(element(by.css("#font1 .text")).getCssValue("font-family")).to.eventually.equal('"Zilla Slab", sans-serif');
+          return webdriver.wait(function() {
+            element(by.css(".mce-floatpanel")).sendKeys(webdriver.Key.END);
+            element(by.xpath("//span[contains(text(), 'Zilla Slab')]")).click();
+
+            expect(element(by.css("#font1 .text")).getCssValue("font-family")).to.eventually.equal('"Zilla Slab", sans-serif');
+          }, 1000);
         });
       });
 
