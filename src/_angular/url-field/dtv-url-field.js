fc0c3c83c6b8e8424eb2280cb7760d9e58be74d6
Stuart Lees <stuartlees@gmail.com>
2021-09-09 14:59:07 -0400

    Show error message if custom url is insecure

--- src/_angular/url-field/dtv-url-field.js
+++ src/_angular/url-field/dtv-url-field.js
@@ -17,6 +17,10 @@
         template: $templateCache.get("_angular/url-field/url-field.html"),
         link: function (scope, element, attrs, ctrl) {
 
+          function isSecureUrl( url ) {
+            return !!( url && url.startsWith( "https://" ) );
+          }
+
           function hasValidExtension(url, fileType) {
             var testUrl = url.toLowerCase(),
               extensions;
@@ -90,9 +94,9 @@
 
             urlRegExp = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i; // jshint ignore:line
 
-            // Add http:// if no protocol parameter exists
+            // Add https:// if no protocol parameter exists
             if (value.indexOf("://") === -1) {
-              value = "http://" + value;
+              value = "https://" + value;
             }
 
             isValid = urlRegExp.test(value);
@@ -107,10 +111,15 @@
             }
 
             if (isValid) {
-              if (scope.fileType === "image") {
-                testImage();
-              } else if (scope.fileType === "video") {
-                testVideo();
+              if (isSecureUrl(value)) {
+                if (scope.fileType === "image") {
+                  testImage();
+                } else if (scope.fileType === "video") {
+                  testVideo();
+                }
+              } else {
+                isValid = false;
+                scope.invalidType = "insecure";
               }
             }
 
