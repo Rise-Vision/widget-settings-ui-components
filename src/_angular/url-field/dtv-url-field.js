04e5ed7c3458fb9bed12bb1cc9da47683805ab54
Stuart Lees <stuartlees@gmail.com>
2021-09-10 10:49:33 -0400

    Disable url input when testing existence of file

--- src/_angular/url-field/dtv-url-field.js
+++ src/_angular/url-field/dtv-url-field.js
@@ -53,28 +53,35 @@
 
                 image.onload = function() {
                   scope.valid = true;
+                  scope.testingFile = false;
                   scope.$apply();
                 };
 
                 image.onerror = function() {
                   scope.valid = false;
+                  scope.testingFile = false;
                   scope.invalidType = "load-fail";
                   scope.$apply();
                 };
 
+                scope.testingFile = true;
                 image.src = scope.url;
               }
             }
           }
 
           function testVideo() {
+            scope.testingFile = true;
+
             $http({
               method: "HEAD",
               url: "https://proxy.risevision.com/" + scope.url
             }).then(function successCallback() {
               scope.valid = true;
+              scope.testingFile = false;
             }, function errorCallback() {
               scope.valid = false;
+              scope.testingFile = false;
               scope.invalidType = "load-fail";
             });
           }
@@ -132,6 +139,8 @@
           scope.forcedValid = false;
           // Validation state
           scope.valid = true;
+
+          scope.testingFile = false;
 
           scope.invalidType = "url";
 
